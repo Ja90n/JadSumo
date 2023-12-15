a91f8af7e11b2eeeada547070981bee6fc50e1b6
Ja90n <jadonvanbeilen@gmail.com>
2023-12-15 17:44:45 +0100

    switched to velocity

--- src/main/java/com/jadonvb/instances/Server.java
+++ src/main/java/com/jadonvb/instances/Server.java
@@ -2,9 +2,11 @@ package com.jadonvb.instances;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.AnvilLoader;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
+import org.apache.velocity.app.Velocity;
 
 import java.nio.file.Path;
 
@@ -22,10 +24,10 @@ public class Server {
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         instanceContainer = instanceManager.createInstanceContainer(new AnvilLoader(Path.of("SimpleSumo")));
 
-        MojangAuth.init();
+        VelocityProxy.enable("0d1yrkMhZEme"); // exposed secret does not matter yet
 
         // Start the server
-        minecraftServer.start("0.0.0.0", 25566);
+        minecraftServer.start("0.0.0.0", 30065);
 
         Game game = new Game(this);
 
