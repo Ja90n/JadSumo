8bf8a77deb259adb78c27165f8b94cdc07d7545b
Ja90n <jadonvanbeilen@gmail.com>
2023-12-16 23:51:15 +0100

    now requests port from velocity

---
+++ src/main/java/com/jadonvb/MessageHandler.java
@@ -0,0 +1,31 @@
+package com.jadonvb;
+
+import com.jadonvb.instances.Server;
+
+public class MessageHandler implements MessageListener {
+
+    private Server server;
+
+    public MessageHandler(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void getMessage(Message message) {
+        switch (message.getType()) {
+            case PORT_REQUEST -> {
+                if (!message.getReceiver().equals(server.getIp())) {
+                    return;
+                }
+                int port = Integer.parseInt(message.getArguments().get(0));
+                if (port == -1) {
+                    server.getLogger().error("Got invallid port!");
+                    server.getLogger().error("Shutting down!");
+                    System.exit(400);
+                }
+                server.setPort(port);
+                server.startServer();
+            }
+        }
+    }
+}
--- src/main/java/com/jadonvb/instances/Server.java
+++ src/main/java/com/jadonvb/instances/Server.java
@@ -1,24 +1,68 @@
 package com.jadonvb.instances;
 
+import com.jadonvb.Client;
+import com.jadonvb.Logger;
+import com.jadonvb.Message;
+import com.jadonvb.MessageTypes;
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.AnvilLoader;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
-import org.apache.velocity.app.Velocity;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.file.Path;
 
 public class Server {
 
     private InstanceContainer instanceContainer;
+    private Client client;
+    private int port;
+    private Logger logger;
+    private String ip;
 
     public Server() {
-        startServer();
+        logger = new Logger("JadSumo");
+        client = new Client();
+        port = getAssignedPort();
     }
 
-    private void startServer() {
+    private String getIP() {
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("google.com", 80));
+            String string = String.valueOf(socket.getLocalAddress());
+            socket.close();
+
+            StringBuilder stringBuilder = new StringBuilder(string);
+            stringBuilder.delete(0,1);
+
+            ip = stringBuilder.toString();
+            return stringBuilder.toString();
+        } catch (IOException ignore) {}
+        return null;
+    }
+
+    private int getAssignedPort() {
+
+        String ip = getIP();
+        if (getIP() == null) {
+            return -1;
+        }
+
+        Message message = new Message();
+        message.setType(MessageTypes.PORT_REQUEST);
+        message.setSender(ip);
+        message.setReceiver("velocity");
+
+        client.sendMessage(message);
+
+        return 0;
+    }
+
+    public void startServer() {
         MinecraftServer minecraftServer = MinecraftServer.init();
 
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
@@ -27,7 +71,7 @@ public class Server {
         VelocityProxy.enable("0d1yrkMhZEme"); // exposed secret does not matter yet
 
         // Start the server
-        minecraftServer.start("0.0.0.0", 30065);
+        minecraftServer.start("0.0.0.0", port);
 
         Game game = new Game(this);
 
@@ -36,5 +80,17 @@ public class Server {
 
     public InstanceContainer getInstanceContainer() {
         return instanceContainer;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public String getIp() {
+        return ip;
     }
 }
